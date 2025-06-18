304390ead989b488e420a52b4795f2e0cf07d55b
UntukPisang-png <marwahdwiparamesti@gmail.com>
2025-06-18 19:48:24 +0800

    Update 2025_04_29_022242_create_pegawais_table.php

--- database/migrations/2025_04_29_022242_create_pegawais_table.php
+++ database/migrations/2025_04_29_022242_create_pegawais_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->integer('nip');
             $table->string('golongan');
             $table->string('pangkat');
+            $table->string('jabatan');
             $table->string('divisi');
             $table->foreignId('user_id');
             $table->timestamps();
